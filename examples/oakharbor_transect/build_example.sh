9d944438586b94c367cd8ce6bbb2ce5b9c08c3aa
Ethan Coon <coonet@ornl.gov>
2026-05-05 12:36:39 -0600

    adds subcycling to transect runs

--- examples/oakharbor_transect/build_example.sh
+++ examples/oakharbor_transect/build_example.sh
@@ -9,11 +9,19 @@ if [ ! -v NTASKS ]; then
     NTASKS=1
 fi
 
+# some options here:
+# DOMAIN_NAME:
+#  - oakharbor_transect.exo is the normal transect
+#  - oakharbor_transect_flat.exo flat transect
+# DOMAIN_FILE:
+#  - domain.lnd.5x1pt_Oakharbor-GRID_navy.nc is the standard one
+#  - domain.lnd.5x1pt_Oakharbor-GRID_navy_elmats.nc is set with identical lat-lon throughout so that forcing is identical
 
 NTASKS=${NTASKS} \
       CASE_NAME=oakharbor_transect.np${NTASKS} \
       ATS_CASE_NAME=oakharbor_transect \
       INPUTDATA_NAME=5x1pt_Oakharbor \
-      DOMAIN_NAME=oakharbor_transect_flat \
+      DOMAIN_NAME=oakharbor_transect \
+      DOMAIN_FILE=domain.lnd.5x1pt_Oakharbor-GRID_navy_elmats.nc \
       ../build_example.sh
 
