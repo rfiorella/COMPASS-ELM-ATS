588bc5c777d3c95c5b4eb553c721a4b7c86bc894
Ethan Coon <coonet@ornl.gov>
2026-05-13 15:34:36 -0600

    working transect simulations for oakharbor

---
+++ examples/oakharbor_transect_flat/build_example.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+if [ -z "${USE_ATS}" ]; then
+    echo "Set USE_ATS prior to running the build_example script"
+    exit 1
+fi
+
+if [ ! -v NTASKS ]; then
+    echo "Setting NTASKS = 1"
+    NTASKS=1
+fi
+
+# some options here:
+# DOMAIN_NAME:
+#  - oakharbor_transect.exo is the normal transect
+#  - oakharbor_transect_flat.exo flat transect
+# DOMAIN_FILE:
+#  - domain.lnd.5x1pt_Oakharbor-GRID_navy.nc is the standard one
+#  - domain.lnd.5x1pt_Oakharbor-GRID_navy_elmats.nc is set with identical lat-lon throughout so that forcing is identical
+
+NTASKS=${NTASKS} \
+      CASE_NAME=oakharbor_transect_flat.np${NTASKS} \
+      ATS_CASE_NAME=oakharbor_transect_flat \
+      INPUTDATA_NAME=5x1pt_Oakharbor \
+      DOMAIN_NAME=oakharbor_transect_flat \
+      DOMAIN_FILE=domain.lnd.5x1pt_Oakharbor-GRID_navy_elmats.nc \
+      ../build_example.sh
+
